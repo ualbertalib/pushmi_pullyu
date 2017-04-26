e6346c24b2ceb10b919c075b228a223fa14415cd
Matt Barnett <matt@sixtyodd.com>
2017-04-26 16:13:46 -0600

    basic connection pooling, aging support, and tests

--- Gemfile
+++ Gemfile
@@ -1,8 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'redis'
-
-gem 'rb-readline'
-
 # Specify your gem's dependencies in pushmi_pullyu.gemspec
 gemspec
--- lib/pushmi_pullyu.rb
+++ lib/pushmi_pullyu.rb
@@ -1,6 +1,8 @@
 require 'pushmi_pullyu/cli'
-require 'pushmi_pullyu/queue'
+require 'pushmi_pullyu/preservation_queue'
 require 'pushmi_pullyu/version'
+require 'active_support'
+require 'active_support/core_ext'
 
 module PushmiPullyu
 end
