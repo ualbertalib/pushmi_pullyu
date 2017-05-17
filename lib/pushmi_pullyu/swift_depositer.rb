03db12072a7532abbad472657b4bfa4a1e6dcf27
astrilet <astrilet@gmail.com>
2017-05-17 13:36:15 -0600

    adding VCR to test cases, small fixes as per comments

--- lib/pushmi_pullyu/swift_depositer.rb
+++ lib/pushmi_pullyu/swift_depositer.rb
@@ -18,13 +18,12 @@ class PushmiPullyu::SwiftDepositer
                                                      authtenant_name: connection[:tenant],
                                                      service_type: 'object-store')
     @swift_container = container
-    @logger = PushmiPullyu.logger
   end
 
   def deposit_file(file_name)
     # check if file exists
     raise "File #{file_name} does not exist" unless File.file?(file_name)
-    file_base_name = File.basename(file_name)
+    file_base_name = File.basename(file_name, '.*')
 
     # calculate file hash
     hash = Digest::MD5.file(file_name).hexdigest
@@ -32,18 +31,15 @@ class PushmiPullyu::SwiftDepositer
     # get container object
     era_container = @swift_connection.container(@swift_container)
 
-    # check that container exits
-    raise "Container #{@swift_container} does not exist" if era_container.nil?
-
     if era_container.object_exists?(file_base_name)
       # if file already exists, update it with new data
-      @logger.debug("File object #{@swift_container}/#{file_base_name} aleary in the swift, updating content")
+      PushmiPullyu.logger.debug("File object #{@swift_container}/#{file_base_name} already in the swift, updating content")
       deposited_file = era_container.object(file_base_name)
       deposited_file.write(File.open(file_name),
                            etag: hash, content_type: 'application/octet-stream')
     else
       # create new deposit file
-      @logger.debug("Creating new file object #{@swift_container}/#{file_base_name} in the swift")
+      PushmiPullyu.logger.debug("Creating new file object #{@swift_container}/#{file_base_name} in the swift")
       deposited_file = era_container.create_object(file_base_name,
                                                    { etag: hash, content_type: 'application/octet-stream' },
                                                    File.open(file_name))
--- spec/pushmi_pullyu/swift_depositer_spec.rb
+++ spec/pushmi_pullyu/swift_depositer_spec.rb
@@ -10,14 +10,36 @@ RSpec.describe PushmiPullyu::SwiftDepositer do
   end
 
   it 'valid connection established' do
-    swift_connection = described_class.new({ username: 'test:tester',
-                                             password: 'testing',
-                                             tenant: 'tester',
-                                             endpoint: 'http://127.0.0.1:8080',
-                                             auth_version: 'v1.0' },
-                                           'ERA')
-    expect(swift_connection).not_to be_nil
-    expect(swift_connection).to be_an_instance_of(described_class)
-    expect(swift_connection.swift_connection).to be_an_instance_of(OpenStack::Swift::Connection)
+    VCR.use_cassette('swift_connect') do
+       swift_connection = described_class.new({ username: 'test:tester',
+                                                password: 'testing',
+                                                tenant: 'tester',
+                                                endpoint: 'http://www.example.com:8080',
+                                                auth_version: 'v1.0' },
+                                              'ERA')
+      expect(swift_connection).not_to be_nil
+      expect(swift_connection).to be_an_instance_of(described_class)
+      expect(swift_connection.swift_connection).to be_an_instance_of(OpenStack::Swift::Connection)
+    end
+  end
+
+  it 'should deposit file' do
+    VCR.use_cassette('swift_deposit') do
+      swift_connection = described_class.new({ username: 'test:tester',
+                                               password: 'testing',
+                                               tenant: 'tester',
+                                               endpoint: 'http://www.example.com:8080',
+                                               auth_version: 'v1.0' },
+                                             'ERA')
+
+      sample_file="#{File.dirname(__FILE__)}/../fixtures/config.yml"
+      deposited_file=swift_connection.deposit_file(sample_file)
+
+      expect(deposited_file).not_to be_nil
+      expect(deposited_file).to be_an_instance_of(OpenStack::Swift::StorageObject)
+      expect(deposited_file.name).to eql 'config'
+      expect(deposited_file.container.name).to eql 'ERA'
+      expect(deposited_file.container.swift).to be_an_instance_of(OpenStack::Swift::Connection)
+    end
   end
 end
