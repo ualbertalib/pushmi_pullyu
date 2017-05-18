4d5a4e5152bee3a18ba884e5c6a82912ac67a8ae
Chris Want <cjwant@gmail.com>
2017-05-18 09:28:34 -0600

    Clean up the public interface of PushmiPullyu::AIP.

--- lib/pushmi_pullyu/aip.rb
+++ lib/pushmi_pullyu/aip.rb
@@ -10,17 +10,18 @@ module PushmiPullyu::AIP
   class SolrFetchError < StandardError; end
 
   def self.create(noid, skip_download: false, clean_work_directories: true)
-    download_aip(noid) unless skip_download
-    bag_aip(noid)
-    tar_bag(noid)
-    destroy_aip_directory(noid) if clean_work_directories
-    # Return the filename of the created file
+    # Return the filename of the created AIP tarball
+    PushmiPullyu::AIP::Creator
+      .new(noid)
+      .run(skip_download: skip_download,
+           clean_work_directories: clean_work_directories)
     aip_filename(noid)
   end
 
-  def self.destroy(noid)
-    destroy_aip_directory(noid)
-    destroy_aip_file(noid)
+  def self.download(noid)
+    # Return the directory of the AIP contents
+    PushmiPullyu::AIP::Downloader.new(noid).run
+    aip_directory(noid)
   end
 
   def self.aip_directory(noid)
@@ -31,36 +32,9 @@ module PushmiPullyu::AIP
     "#{aip_directory(noid)}/#{noid}.tar"
   end
 
-  private
-
-  def destroy_aip_file(noid)
-    filename = aip_filename(noid)
-    FileUtils.rm(filename) if File.exist?(filename)
-  end
-
-  def destroy_aip_directory(noid)
-    directory = aip_directory(noid)
-    return unless File.exist?(directory)
-    PushmiPullyu.logger.info("#{noid}: Nuking directories ...")
-    FileUtils.rm_rf(directory)
-  end
-
-  def download_aip(noid)
-    PushmiPullyu::AIP::Downloader.run(noid)
-  end
-
-  def bag_aip(noid)
-    bag = BagIt::Bag.new(aip_directory(noid))
-    bag.manifest!
-    raise PushmiPullyu::BagInvalid unless bag.valid?
-  end
-
-  def tar_bag(noid)
-    destroy_aip_file(noid)
-    Dir.chdir(aip_directory(noid)) do
-      File.open(aip_filename(noid), 'wb') do |tar|
-        Archive::Tar::Minitar.pack(noid, tar)
-      end
+  def self.destroy(noid)
+    [aip_directory(noid), aip_filename(noid)].each do |path|
+      FileUtils.rm_rf(path) if File.exist?(path)
     end
   end
 end
