773f8f43574136b85b145f642bf614a1b3506e23
astrilet <astrilet@gmail.com>
2017-05-18 10:15:22 -0600

    Added code removed during merge :(

--- lib/pushmi_pullyu/cli.rb
+++ lib/pushmi_pullyu/cli.rb
@@ -164,11 +164,16 @@ class PushmiPullyu::CLI
       # add additional information about the error context to errors that occur while processing this item.
       Rollbar.scoped(noid: item) do
         begin
-        # 3. Retrieve GenericFile data in fedora
-        # 4. creation of AIP
-        # 5. bagging and tarring of AIP
-        # 6. Push bag to swift API
-        # 7. Log successful preservation event to log files
+          # 3. Retrieve GenericFile data in fedora
+          # 4. creation of AIP
+          # 5. bagging and tarring of AIP
+
+          # 6. Push bag to swift API
+          file_to_deposit = './pushmi_pullyu'
+          @storage.deposit_file(file_to_deposit)
+          logger.debug("Deposited file into the swift storage #{file_to_deposit}")
+
+          # 7. Log successful preservation event to log files
         rescue => e
           Rollbar.error(e)
           # TODO: we could re-raise here and let the daemon die on any preservation error, or just log the issue and
