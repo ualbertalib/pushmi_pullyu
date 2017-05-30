bfb794536d3279a164d8ef496e6da4d6e51525f7
Murny <murnagha@gmail.com>
2017-05-30 11:57:13 -0600

    Bring swift depositer spec inline with all the other specs

--- spec/pushmi_pullyu/swift_depositer_spec.rb
+++ spec/pushmi_pullyu/swift_depositer_spec.rb
@@ -1,42 +1,44 @@
 require 'spec_helper'
 
 RSpec.describe PushmiPullyu::SwiftDepositer do
-  it 'deposits new file' do
-    VCR.use_cassette('swift_new_deposit') do
-      swift_depositer = PushmiPullyu::SwiftDepositer.new(username: 'test:tester',
-                                                         password: 'testing',
-                                                         tenant: 'tester',
-                                                         endpoint: 'http://www.example.com:8080',
-                                                         auth_version: 'v1.0')
-      sample_file = 'spec/fixtures/config.yml'
+  describe '#deposit_file' do
+    it 'deposits new file' do
+      VCR.use_cassette('swift_new_deposit') do
+        swift_depositer = PushmiPullyu::SwiftDepositer.new(username: 'test:tester',
+                                                           password: 'testing',
+                                                           tenant: 'tester',
+                                                           endpoint: 'http://www.example.com:8080',
+                                                           auth_version: 'v1.0')
+        sample_file = 'spec/fixtures/config.yml'
 
-      deposited_file = swift_depositer.deposit_file(sample_file, 'ERA')
+        deposited_file = swift_depositer.deposit_file(sample_file, 'ERA')
 
-      expect(deposited_file).not_to be_nil
-      expect(deposited_file).to be_an_instance_of(OpenStack::Swift::StorageObject)
-      expect(deposited_file.name).to eql 'config'
-      expect(deposited_file.container.name).to eql 'ERA'
+        expect(deposited_file).not_to be_nil
+        expect(deposited_file).to be_an_instance_of(OpenStack::Swift::StorageObject)
+        expect(deposited_file.name).to eql 'config'
+        expect(deposited_file.container.name).to eql 'ERA'
+      end
     end
-  end
 
-  it 'updates existing file' do
-    VCR.use_cassette('swift_update_deposit') do
-      swift_depositer = PushmiPullyu::SwiftDepositer.new(username: 'test:tester',
-                                                         password: 'testing',
-                                                         tenant: 'tester',
-                                                         endpoint: 'http://www.example.com:8080',
-                                                         auth_version: 'v1.0')
+    it 'updates existing file' do
+      VCR.use_cassette('swift_update_deposit') do
+        swift_depositer = PushmiPullyu::SwiftDepositer.new(username: 'test:tester',
+                                                           password: 'testing',
+                                                           tenant: 'tester',
+                                                           endpoint: 'http://www.example.com:8080',
+                                                           auth_version: 'v1.0')
 
-      sample_file = 'spec/fixtures/config_with_envs.yml'
+        sample_file = 'spec/fixtures/config_with_envs.yml'
 
-      # Deposits file twice, check that it only gets added once to the container
-      expect do
-        first_deposit = swift_depositer.deposit_file(sample_file, 'ERA')
-        second_deposit = swift_depositer.deposit_file(sample_file, 'ERA')
+        # Deposits file twice, check that it only gets added once to the container
+        expect do
+          first_deposit = swift_depositer.deposit_file(sample_file, 'ERA')
+          second_deposit = swift_depositer.deposit_file(sample_file, 'ERA')
 
-        expect(first_deposit.name).to eq(second_deposit.name)
-        expect(first_deposit.container.name).to eq(second_deposit.container.name)
-      end.to change { swift_depositer.swift_connection.container('ERA').count.to_i }.by(1)
+          expect(first_deposit.name).to eq(second_deposit.name)
+          expect(first_deposit.container.name).to eq(second_deposit.container.name)
+        end.to change { swift_depositer.swift_connection.container('ERA').count.to_i }.by(1)
+      end
     end
   end
 end
