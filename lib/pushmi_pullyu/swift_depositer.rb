2aa716f78cd071797b1874250d287bfa7e44222a
Alex Strilets <astrilet@gmail.com>
2017-06-13 11:13:59 -0600

    Chage code to write file content during file creation (#77)
    * Chage code to write file content during file creation
    
    * Changes to PR as requested

--- lib/pushmi_pullyu/swift_depositer.rb
+++ lib/pushmi_pullyu/swift_depositer.rb
@@ -23,12 +23,6 @@ class PushmiPullyu::SwiftDepositer
 
     era_container = swift_connection.container(swift_container)
 
-    deposited_file = if era_container.object_exists?(file_base_name)
-                       era_container.object(file_base_name)
-                     else
-                       era_container.create_object(file_base_name)
-                     end
-
     # Add swift metadata with in accordance to AIP spec:
     # https://docs.google.com/document/d/154BqhDPAdGW-I9enrqLpBYbhkF9exX9lV3kMaijuwPg/edit#
     metadata = {
@@ -41,9 +35,15 @@ class PushmiPullyu::SwiftDepositer
     # ruby-openstack wants all keys of the metadata to be named like "X-Object-Meta-{{Key}}", so update them
     metadata.transform_keys! { |key| "X-Object-Meta-#{key}" }
 
-    deposited_file.write(File.open(file_name),
-                         { 'etag' => checksum,
-                           'content-type' => 'application/x-tar' }.merge(metadata))
+    headers = { 'etag' => checksum,
+                'content-type' => 'application/x-tar' }.merge(metadata)
+
+    if era_container.object_exists?(file_base_name)
+      deposited_file = era_container.object(file_base_name)
+      deposited_file.write(File.open(file_name), headers)
+    else
+      deposited_file = era_container.create_object(file_base_name, headers, File.open(file_name))
+    end
 
     deposited_file
   end
