5300947870c5e7d474c839f9432352613c2a6c11
Chris Want <cjwant@gmail.com>
2018-03-19 10:06:06 -0600

    Update the database config and examples.

--- lib/pushmi_pullyu.rb
+++ lib/pushmi_pullyu.rb
@@ -59,7 +59,8 @@ module PushmiPullyu
     database: {
       encoding: 'utf8',
       pool: ENV['RAILS_MAX_THREADS'] || 5,
-      url: ENV['PUSHMI_PULLYU_DATABASE_URL'] || ENV['DATABASE_URL'] || ENV['JUPITER_DATABASE_URL']
+      url: ENV['PUSHMI_PULLYU_DATABASE_URL'] || ENV['DATABASE_URL'] || ENV['JUPITER_DATABASE_URL'],
+      database: 'jupiter_development'
     }
   }.freeze
 
