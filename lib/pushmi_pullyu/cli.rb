e00e163369bba5a7dab554628d65203dc37fe286
Weiwei Shi <weiwei.shi@ualberta.ca>
2018-11-29 14:48:12 -0700

    make use_exception_level_filter a global setting

--- lib/pushmi_pullyu/cli.rb
+++ lib/pushmi_pullyu/cli.rb
@@ -38,7 +38,7 @@ class PushmiPullyu::CLI
       end
     # rubocop:disable Lint/RescueException
     rescue Exception => e
-      Rollbar.error(e, use_exception_level_filters: true)
+      Rollbar.error(e)
       raise e
     end
     # rubocop:enable Lint/RescueException
@@ -59,6 +59,7 @@ class PushmiPullyu::CLI
     Rollbar.configure do |config|
       config.enabled = false unless options[:rollbar][:token].present?
       config.access_token = options[:rollbar][:token]
+      config.use_exception_level_filters = true
       config.exception_level_filters['IOError'] = 'ignore'
       # add a filter after Rollbar has built the error payload but before it is delivered to the API,
       # in order to strip sensitive information out of certain error messages
