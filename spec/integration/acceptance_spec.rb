1f5941fe3fa601a2c45e69db9eee2a5adcc842cf
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-02-20 13:53:38 -0700

    Fix acceptance test
    Fetching the data directly from Jupiter broke this test since the format
    has now changed. The new vcr cassette reflects the new use of the AIP
    API.

--- spec/integration/acceptance_spec.rb
+++ spec/integration/acceptance_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
+require 'json'
 
 RSpec.describe 'Acceptance test', type: :feature do
   let(:workdir) { 'tmp/spec' }
-  let(:noid) { '6841cece-41f1-4edf-ab9a-59459a127c77' }
-  let(:aip_folder) { "#{workdir}/#{noid}" }
+  let(:uuid) { '40fd4906-9618-41d6-8180-2880f3496520' }
+  let(:type) { 'items' }
+  let(:json_redis_input) do
+    {
+      uuid: uuid,
+      type: type
+    }
+  end
+  let(:aip_folder) { "#{workdir}/#{uuid}" }
   let(:aip_file) { "#{aip_folder}.tar" }
   let(:log_folder) { "#{workdir}/log" }
 
@@ -11,15 +19,13 @@ RSpec.describe 'Acceptance test', type: :feature do
   let!(:redis) { Redis.new }
 
   before do
-    # setup redis and add noid to queue
-    redis.zadd 'test:pmpy_queue', 10, noid
+    # setup redis and add uuid to queue
+    redis.zadd 'test:pmpy_queue', 10, json_redis_input.to_json
 
     FileUtils.mkdir_p(workdir)
     FileUtils.mkdir_p(log_folder)
 
     allow(PushmiPullyu::Logging.logger).to receive(:info)
-    allow(PushmiPullyu::AIP::User)
-      .to receive(:find).with(2705).and_return(OpenStruct.new(email: 'admin@example.com'))
   end
 
   after do
@@ -29,13 +35,17 @@ RSpec.describe 'Acceptance test', type: :feature do
   end
 
   # this is basically testing exactly what the `PushmiPullyu::CLI#run_preservation_cycle` method does
-  it 'successfully gets NOID off queue, fetches data from fedora/database, creates AIP and uploads to Swift' do
+  it 'successfully gets entity off queue, fetches data from jupiter, creates AIP and uploads to Swift' do
     cli = PushmiPullyu::CLI.instance
     cli.parse(['-C', 'spec/fixtures/config.yml', '-W', workdir])
 
-    item = cli.send(:queue).wait_next_item
+    entity_json = JSON.parse(cli.send(:queue).wait_next_item)
+    entity = {
+      type: entity_json['type'],
+      uuid: entity_json['uuid']
+    }
 
-    expect(item).to eq noid
+    expect(entity[:uuid]).to eq uuid
 
     # Should not exist yet
     expect(File.exist?(aip_folder)).to eq(false)
@@ -43,8 +53,11 @@ RSpec.describe 'Acceptance test', type: :feature do
     expect(File.exist?("#{log_folder}/preservation_events.log")).to eq(false)
 
     # Download AIP from Fedora, bag and tar AIP directory and cleanup after block code
-    VCR.use_cassette('aip_download_and_swift_upload') do
-      PushmiPullyu::AIP.create(item) do |aip_filename|
+    VCR.use_cassette('aip_download_and_swift_upload', erb: {
+      file_path_1: './spec/fixtures/storage/k7/hb/k7hb4VEsfoPXTab1W5iB6yXP',
+      file_path_2: './spec/fixtures/storage/jf/KQ/jfKQSzhKRHrnfYAVY38htiZo'
+    }) do
+      PushmiPullyu::AIP.create(entity) do |aip_filename|
         expect(aip_file).to eq(aip_filename)
         # aip file and folder should have been created by the creator
         expect(File.exist?(aip_folder)).to eq(true)
@@ -54,10 +67,10 @@ RSpec.describe 'Acceptance test', type: :feature do
         deposited_file = cli.send(:swift).deposit_file(aip_filename, PushmiPullyu.options[:swift][:container])
 
         expect(deposited_file).to be_an_instance_of(OpenStack::Swift::StorageObject)
-        expect(deposited_file.name).to eql noid
+        expect(deposited_file.name).to eql uuid
         expect(deposited_file.container.name).to eql PushmiPullyu.options[:swift][:container]
         expect(deposited_file.metadata['project']).to eql PushmiPullyu.options[:swift][:container]
-        expect(deposited_file.metadata['project-id']).to eql noid
+        expect(deposited_file.metadata['project-id']).to eql uuid
         expect(deposited_file.metadata['aip-version']).to eql '1.0'
         expect(deposited_file.metadata['promise']).to eql 'bronze'
 
@@ -67,41 +80,31 @@ RSpec.describe 'Acceptance test', type: :feature do
     end
 
     expect(File.exist?("#{log_folder}/preservation_events.log")).to eq(true)
-
     log_details = <<~HEREDOC
-      #{noid} was successfully deposited into Swift Storage!
+      #{uuid} was successfully deposited into Swift Storage!
       Here are the details of this preservation event:
-      \tNOID: '#{noid}'
-      \tTimestamp of Completion: 'Wed, 07 Jun 2017 20:55:45 GMT'
-      \tAIP Checksum: '2752dc32b7a56b42aee3dd4d235a24a2'
-      \tMetadata: {"project-id"=>"#{noid}", "aip-version"=>"1.0", "promise"=>"bronze", "project"=>"ERA"}
+      \tUUID: '#{uuid}'
+      \tTimestamp of Completion: 'Thu, 20 Feb 2020 20:23:00 GMT'
+      \tAIP Checksum: '7ad57970a87a66efad09aae29965c96a'
+      \tMetadata: {"project-id"=>"#{uuid}", "aip-version"=>"1.0", "promise"=>"bronze", "project"=>"ERA"}
       \tFile Details:
     HEREDOC
 
     file_details_one = <<~HEREDOC
-      \t\t{"fileset_uuid": "01bb1b09-974d-478b-8826-2c606a447606",
+      \t\t{"fileset_uuid": "3cbc75f9-7bad-4d62-962e-911f911bd70e",
       \t\t"details": {
-      \t\t\t"file_name": "theses.jpg",
-      \t\t\t"file_type": "jpg",
-      \t\t\t"file_size": 53678
+      \t\t\t"file_name": "image-sample.jpeg",
+      \t\t\t"file_type": "jpeg",
+      \t\t\t"file_size": 12086
       \t\t}}
     HEREDOC
 
     file_details_two = <<~HEREDOC
-      \t\t{"fileset_uuid": "837977d6-de61-49ea-a912-a65af5c9005e",
+      \t\t{"fileset_uuid": "8cf761ce-5222-405a-aee1-f019f860e4ba",
       \t\t"details": {
-      \t\t\t"file_name": "image-sample.jpeg",
-      \t\t\t"file_type": "jpeg",
-      \t\t\t"file_size": 12401
-      \t\t}}
-    HEREDOC
-
-    file_details_three = <<~HEREDOC
-      \t\t{"fileset_uuid": "856444b6-8dd5-4dfa-857d-435e354a2ead",
-      \t\t"details": {
-      \t\t\t"file_name": "era-logo.png",
-      \t\t\t"file_type": "png",
-      \t\t\t"file_size": 5612
+      \t\t\t"file_name": "theses.jpg",
+      \t\t\t"file_type": "jpg",
+      \t\t\t"file_size": 51959
       \t\t}}
     HEREDOC
 
@@ -110,7 +113,6 @@ RSpec.describe 'Acceptance test', type: :feature do
     expect(log_file).to include(log_details)
     expect(log_file).to include(file_details_one)
     expect(log_file).to include(file_details_two)
-    expect(log_file).to include(file_details_three)
 
     # aip file and folder should have been cleaned up
     expect(File.exist?(aip_folder)).to eq(false)
